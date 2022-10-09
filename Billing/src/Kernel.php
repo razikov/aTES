4fc3f52f6d07723acfbddfb98b2fdf2e73a723ab
Разиков Алексей <razikov88@yandex.ru>
2022-10-09 18:25:43 +0300

    created billing app template

---
+++ Billing/src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Razikov\AtesBilling;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
